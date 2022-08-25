0fe9fbc3a2693a5de67a639847b9c016642ce53b
anilo2021 <95632935+anilo2021@users.noreply.github.com>
2022-08-25 16:15:14 +0000

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -15,7 +15,13 @@ terraform {
 
 # Provider Block
 provider "azurerm" {
- features {}          
+  
+  version = ">= 2.0"
+  subscription_id = "ee902f6d-01a9-4a32-8c5f-af9403e6c766"
+  tenant_id = "abec7981-3822-4685-98b0-533aca20c2ed"
+  client_id = "b63d410e-e633-4271-847d-0a6e1b67edc4"
+  client_secret = "CEG8Q~T.hAzULPXkwqurz1yjTzeLbEzQfj4UCb.J"
+  features {}          
 }
 
 # Random String Resource
